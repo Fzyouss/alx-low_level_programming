5d13cb22b978ac3245880a816e6cb3d72b48d5fc
fzyouss <fatima.ezahra.youssefi3@gmail.com>
2023-07-13 13:09:33 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stidio.h>
+#include <stdio.h>
 /**
 *main - main block
 *Return: 0
 */
 int main(void)
 {
-puts("\nProgramming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
