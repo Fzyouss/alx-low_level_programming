f5e73eaddffc4d16447e7f261677c5216f0ca2e2
fzyouss <fatima.ezahra.youssefi3@gmail.com>
2023-07-18 03:31:06 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ *Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	while (c = 'a'; c <= 'z'; c++)
+	_putchar(c);
+	_putchar('\n');
+}
